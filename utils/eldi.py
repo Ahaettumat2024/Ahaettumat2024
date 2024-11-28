edc7881bf1464e9bbc54a898d07037518ad81f4d
RagnarJohannsson <155158118+RagnarJohannsson@users.noreply.github.com>
2024-11-28 09:46:33 +0000

    Update eldi.py

--- utils/eldi.py
+++ utils/eldi.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-EVENTS_PER_YEAR = 0.57 # 1meðalfjöldi atburða á ári
+EVENTS_PER_YEAR = 0.57 # 1/meðalfjöldi atburða á ári
 SIZE_PROPORTION = 0.67 # hlutfall snemmbúimma vs. síðbúinna
 
 LATE_RETURNS_PROP = 0.0025 # fjöldi síðbúinna stokulaxa sem snúa aftur
