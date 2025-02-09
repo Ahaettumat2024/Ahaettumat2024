e643fb6f093795fb642fc7c30466de197d509b99
Ahaettumat2024 <ahaettumat@gmail.com>
2025-02-09 20:55:01 +0100

    Update dreifing.py

--- utils/dreifing.py
+++ utils/dreifing.py
@@ -8,62 +8,50 @@ LATE_PROPORTION = 0.2
 LATE_LENGTH = 240
 LATE_LENGTH_2 = LATE_LENGTH*(1-LATE_PROPORTION)/LATE_PROPORTION
 
-EARLY_PROPORTION = 0.2
-EARLY_LENGTH = 80
+EARLY_PROPORTION = 0.5
+EARLY_LENGTH = 100
 EARLY_LENGTH_2 = EARLY_LENGTH*(1-EARLY_PROPORTION)/EARLY_PROPORTION
 
 def lateDistribution(x):
     # Reiknar dreifingu síbúinna stokulaxa
     if x > 0:
-        return np.exp(-(-x/LATE_LENGTH_2)**2)
+        return np.exp(-(x/LATE_LENGTH_2)**2)
     else:
         return np.exp(-(x/LATE_LENGTH)**2)
     
 def earlyDistribution(x):
     # Reiknar dreifingu snemmbúinna stokulaxa
     if x > 0:
-        return np.exp(-(-x/EARLY_LENGTH_2)**2)
+        return np.exp(-(x/EARLY_LENGTH_2)**2)
     else:
         return np.exp(-(x/EARLY_LENGTH)**2)
 
 @st.cache_data
-def getEarlyFarmedDistribution(stofnstaerdir, farmNo):
-    # Reiknar hlutfall snemmbúinna stokulaxa sem fer í hverja á 
-    farmPosition = st.session_state['eldi'].loc[farmNo,'staðsetning']
-    distances = st.session_state['rivers']['fjarlægð'].to_numpy()
-    adjustedDistances = distances - farmPosition
-    adjustedDistances = pd.Series(adjustedDistances)
+def getEarlyFarmedDistribution(farmNo):
+    farmName = st.session_state['eldi'].loc[farmNo,'Stytting']
+    distances = st.session_state['distances'][farmName]
 
-    #adjustedDistances = pd.Series(np.append(adjustedDistances,0))
-    #stofnstaerdir = np.append(stofnstaerdir.to_numpy(),100)
-
-    adjustedDistances = adjustedDistances.map(lambda x : x + 2430 if x < -1215 else (x - 2430 if x > 1215 else x))
-    distancesProb = adjustedDistances.map(earlyDistribution).to_numpy()
-    distancesProb = distancesProb/distancesProb.sum()
-    stofnstaerdirProb = np.sqrt(stofnstaerdir)/(np.sum(np.sqrt(stofnstaerdir)))
-    stofnstaerdirProb = stofnstaerdirProb/stofnstaerdirProb.sum()
+    distancesProb = distances.map(earlyDistribution).to_numpy()
+    stofnstaerdirProb = np.sqrt(st.session_state['rivers']['expMedal10'].to_numpy())
+    stofnstaerdirProb = stofnstaerdirProb/np.max(stofnstaerdirProb)
 
     distribution = distancesProb*stofnstaerdirProb
     distribution = distribution/np.sum(distribution)
-    #distribution = distribution[:-1]
     return distribution
 
 @st.cache_data
-def getLateFarmedDistribution(stofnstaerdir, farmNo):
+def getLateFarmedDistribution(farmNo):
     # Reiknar hlutfall síbúinna stokulaxa sem fer í hverja á 
-    farmPosition = st.session_state['eldi'].loc[farmNo,'staðsetning']
-    distances = st.session_state['rivers']['fjarlægð']
-    adjustedDistances = distances - farmPosition
+    farmName = st.session_state['eldi'].loc[farmNo,'Stytting']
+    distances = st.session_state['distances'][farmName]
 
-    adjustedDistances = adjustedDistances.map(lambda x : x + 2430 if x < -1215 else (x - 2430 if x > 1215 else x))
-    distancesProb = adjustedDistances.map(lateDistribution).to_numpy()
-    distancesProb = distancesProb/distancesProb.sum()
-    stofnstaerdirProb = np.sqrt(stofnstaerdir)/(np.sum(np.sqrt(stofnstaerdir)))
-    stofnstaerdirProb = stofnstaerdirProb/stofnstaerdirProb.sum()
+    distancesProb = distances.map(lateDistribution).to_numpy()
+    distancesProb = distancesProb/np.max(distancesProb)
+    stofnstaerdirProb = np.sqrt(st.session_state['rivers']['expMedal10'].to_numpy())
+    stofnstaerdirProb = stofnstaerdirProb/np.max(stofnstaerdirProb)
 
     distribution = distancesProb*stofnstaerdirProb
     distribution = distribution/np.sum(distribution)
-
     return distribution
 
 @st.cache_data
@@ -75,10 +63,10 @@ def getResults(stofnstaerdir, farmEarlyReturns, farmLateReturns, ITERS):
         print(i)
         for j in farmEarlyReturns.columns:
             if farmEarlyReturns.loc[i,j] > 0:
-                results.loc[i] += farmEarlyReturns.loc[i,j]*getEarlyFarmedDistribution(results.loc[i],j)
+                results.loc[i] += farmEarlyReturns.loc[i,j]*getEarlyFarmedDistribution(j)
             if farmLateReturns.loc[i,j] > 0:
-                results.loc[i] += farmLateReturns.loc[i,j]*getLateFarmedDistribution(results.loc[i],j)
-        results.loc[i,:] = 100*(results.loc[i,:].to_numpy()-stofn.loc[i,:].to_numpy())/results.loc[i,:].clip(lower = 1).to_numpy()
+                results.loc[i] += farmLateReturns.loc[i,j]*getLateFarmedDistribution(j)
+        results.loc[i,:] = 100*(results.loc[i,:].to_numpy()-stofn.loc[i,:].to_numpy())/(results.loc[i,:].clip(lower = 1)).to_numpy()
     return results
 
 ## Oþarfi að breyta
@@ -86,11 +74,10 @@ def plotDistribution(ax, type, farm):
     # Plottar dreyfingu
     farmDict = pd.Series(st.session_state['eldi'].index, index=st.session_state['eldi']['Nafn'].values).to_dict()
     farmNo = farmDict[farm]
-    stofnstaerdir = st.session_state['rivers']['Meðalfjöldi']
     if type == 'Snemmbúnir':
-        distribution = getEarlyFarmedDistribution(stofnstaerdir, farmNo)
+        distribution = getEarlyFarmedDistribution(farmNo)
     else:
-        distribution = getLateFarmedDistribution(stofnstaerdir, farmNo)
+        distribution = getLateFarmedDistribution(farmNo)
 
     ax.bar(st.session_state['rivers']['nafn'], distribution)
     ax.set_xticks(ax.get_xticks())
