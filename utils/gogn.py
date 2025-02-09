0052af7cfd6923bdd8d3839ad87a4c2ac7327a49
Ahaettumat2024 <ahaettumat@gmail.com>
2025-02-09 20:54:06 +0100

    Update gogn.py

--- utils/gogn.py
+++ utils/gogn.py
@@ -3,10 +3,12 @@ import pandas as pd
 
 
 def getRivers():
-    rivers = pd.read_csv('./data/ar.csv')
+    rivers = pd.read_csv('./data/ahaetta.csv', sep=';', decimal=',')                 
+    rivers = rivers[rivers.ath != 'ut']
     if 'std' not in rivers.columns:
-        rivers['std'] = 10
+        rivers['std'] = 0.5
     rivers.sort_values(by=['fjarlægð'],inplace=True)
+
     return rivers
 
 def getFarms():
@@ -14,5 +16,9 @@ def getFarms():
     farms.sort_values(by=['staðsetning'],inplace=True)
     return farms
 
-def getSetings():
-    return None
+def getDistances():
+    distances = pd.read_csv('./data/distances.csv')
+    distances.sort_values(by=['Arn'],inplace=True)
+    rivers = getRivers()
+    distances.index = rivers['nafn']
+    return distances
