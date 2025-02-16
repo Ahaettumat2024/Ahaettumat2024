035bf6174c9c56ea956da1a427a39628b09ea776
Ahaettumat2024 <ahaettumat@gmail.com>
2025-02-16 16:23:24 +0100

    Update ahaettumat.py

--- ahaettumat.py
+++ ahaettumat.py
@@ -22,8 +22,8 @@ if 'distances' not in st.session_state:
 if 'calc' not in st.session_state:
     st.session_state['calc'] = False
 
-st.header('Áhættumat 2024')
-tab1, tab2, tab3, tab4 = st.tabs(['Laxastofn', 'Strok', 'Dreifing', 'Niðurstaða'])
+st.header('GIRAF 2025')
+tab1, tab2, tab3, tab4 = st.tabs(['Salmon stocks', 'Escape events', 'Distribution', 'Results'])
 
 ## Run calculation
 if not st.session_state['calc']:
@@ -48,14 +48,14 @@ if not st.session_state['calc']:
 
 ## Laxastofn
 river = tab1.selectbox(
-    "Veldu á",
-    ['Heild'] + list( st.session_state['rivers']['nafn']),
+    "Select a river",
+    ['Total'] + list( st.session_state['rivers']['nafn']),
 )
 riverPlotType = tab1.pills(
-    "Tegund grafs",
-    options=['Tímalína', 'Stuðlarit'],
+    "Type of plot",
+    options=['Timeline', 'Histogram'],
     selection_mode="single",
-    default = 'Tímalína'
+    default = 'Timeline'
 )
 f, ax = plt.subplots()
 plotStofnstaerdir(ax, st.session_state['stofnar'], river, riverPlotType, ITERS)
@@ -63,10 +63,7 @@ tab1.pyplot(f)
 
 ## Strok
 col31,col32 = tab2.columns([3, 1])
-eldismagn = col32.expander('Eldismagn')
-poisson = col32.expander('Meðaltímalengd milli atburða')
-staerd = col32.expander('Stærð atburða')
-eldisstudlar = col32.expander('Eldisstuðlar')
+eldismagn = col32.expander('Farm sizes')
 
 col311, col312, col313 = col31.columns([1, 1, 1])
 
@@ -75,20 +72,20 @@ for eldisIdx in st.session_state['eldi'].index:
     eldismagn.metric(row['Nafn'], row['Stock'])
     #eldismagn.slider(row['Nafn'], 0.0, row['max'], row['Stock'], step = 0.5, key = row['Stytting'], on_change=updateEldi, args=(row['Stytting'],))
 
-col311.metric('Meðalfjöldi atburða á ári',  st.session_state['escSchedule'].mean())
-col312.metric('Meðalfjöldi stroka á ári', round(( st.session_state['farmNumbersEarly'].to_numpy().sum()+ st.session_state['farmNumbersLate'].to_numpy().sum())/1000))
-col313.metric('Strok per þús. tonn', round(( st.session_state['farmNumbersEarly'].to_numpy().sum()+ st.session_state['farmNumbersLate'].to_numpy().sum())/(1000*1000*st.session_state['eldi'].loc[:,'Stock'].sum()),1))
+col311.metric('Average number of events per year',  st.session_state['escSchedule'].mean())
+col312.metric('Average escapes per year', round(( st.session_state['farmNumbersEarly'].to_numpy().sum()+ st.session_state['farmNumbersLate'].to_numpy().sum())/1000))
+col313.metric('Escapes per 1000 tons', round(( st.session_state['farmNumbersEarly'].to_numpy().sum()+ st.session_state['farmNumbersLate'].to_numpy().sum())/(1000*1000*st.session_state['eldi'].loc[:,'Stock'].sum()),1))
 
 f2, ax2 = plt.subplots()
 eldi = col31.selectbox(
-    "Veldu eldisstað",
-    ['Heild'] + list( st.session_state['eldi']['Nafn']),
+    "Select a farm",
+    ['Total'] + list( st.session_state['eldi']['Nafn']),
 )
 eldiPlotType = col31.pills(
-    "Graf",
-    options=['Atburðir', 'Strokfjöldi', 'Endurkomulaxar'],
+    "Plot",
+    options=['Events', 'Escape numbers', 'Returners'],
     selection_mode="single",
-    default = 'Atburðir'
+    default = 'Events'
 )
 ax2 = plotEldi(ax2, eldi, eldiPlotType, st.session_state['farmEventsEarly'], st.session_state['farmEventsLate'], st.session_state['farmNumbersEarly'], st.session_state['farmNumbersLate'], st.session_state['farmEarlyReturns'], st.session_state['farmLateReturns'])
 col31.pyplot(f2)
@@ -97,15 +94,15 @@ col31.pyplot(f2)
 
 f3, ax3 = plt.subplots()
 eldi2 = tab3.selectbox(
-    "Veldu eldisstað",
+    "Select a farm",
     list( st.session_state['eldi']['Nafn']),
     key = 'eldi2'
 )
 dreifingPlotType = tab3.pills(
-    "Graf",
-    options=['Snemmbúnir', 'Síðbúnir'],
+    "Plot",
+    options=['Early', 'Late'],
     selection_mode="single",
-    default = 'Snemmbúnir'
+    default = 'Early'
 )
 plotDistribution(ax3, dreifingPlotType, eldi2)
 tab3.pyplot(f3)
@@ -113,10 +110,10 @@ tab3.pyplot(f3)
 
 
 ## Niðurstaða
-f4, ax4 = plt.subplots()
+f4, ax4 = plt.subplots(figsize=[11,6])
 river2 = tab4.selectbox(
-    "Veldu á",
-    ['Heild'] + list( st.session_state['rivers']['nafn']),
+    "Select a river",
+    ['Total'] + list( st.session_state['rivers']['nafn']),
     key = 'river2'
 )
 plotResult(ax4, river2, st.session_state['results'])
